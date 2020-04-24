672c290a947de4d9e81dc273376e1ce15f758ca6
thomasbusaan <thomas.busaan@student.odisee.be>
2020-04-24 13:25:30 +0200

    Added home acceptatietest

--- travelbase/src/test/java/be/odisee/travelbase/TravelbaseApplicationTests.java
+++ travelbase/src/test/java/be/odisee/travelbase/TravelbaseApplicationTests.java
@@ -1,35 +1,10 @@
 package be.odisee.travelbase;
 
-import be.odisee.travelbase.controller.HomeController;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.*;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+@RunWith(Cucumber.class)
+@CucumberOptions(format={"pretty", "html:target/cucumber"},tags={"~@skip"})
 public class TravelbaseApplicationTests {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void home() throws Exception {
-
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("home"))
-                .andExpect(content().string(containsString("Welcome to Travelbase")))
-        ;
-    }
 }
-
-
